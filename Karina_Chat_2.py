1e6b890827858403c68bad40a2777337c9c45517
JWalldorf <jens.walldorf@gmx.de>
2025-05-15 20:15:34 +0200

    Update Karina_Chat_2.py

--- Karina_Chat_2.py
+++ Karina_Chat_2.py
@@ -445,8 +445,7 @@ if "startzeit" not in st.session_state:
 # st.write("Szenario:", st.session_state.diagnose_szenario)
 # st.write("Features:", st.session_state.diagnose_features)
 # st.write("Prompt:", st.session_state.SYSTEM_PROMPT)
-
-speichere_gpt_feedback_in_supabase()
+# speichere_gpt_feedback_in_supabase()
 
 
 # Chat-Verlauf starten
