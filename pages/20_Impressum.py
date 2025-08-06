252c4e2c2de97264ae050d4194c9ca3709f92f81
JWalldorf <jens.walldorf@gmx.de>
2025-08-06 17:48:53 +0200

    Update 20_Impressum.py

--- pages/20_Impressum.py
+++ pages/20_Impressum.py
@@ -19,18 +19,18 @@ def show_impressum():
     E-Mail: jens.walldorf@uk-halle.de  
 
     ---
+    ⚠️ Bitte beachten Sie, dass Sie mit einem **KI-basierten, simulierten Patientinnenmodell** kommunizieren.
     
     Es handelt sich bei diesen Seiten um **ein experimentelles Patientenmodell ausschließlich zu Lehrzwecken**. Wichtiges Lernziel der App ist es unter anderem, die Limitationen (**Fehlinterpretationen, falsche Informationen**) in der von der KI generierten Antworten zu identifizieren.
     
     ⚠️ Die von der KI generierten Informationen aus dieser App können fehlerhaft sein! Alle Informationen, die von der KI mitgeteilt werden, müssen mit geeigneter Fachliteratur abgeglichen werden bzw. können Diskussiongrundlage im Studentenunterricht sein.
     
-    ⚠️ Bitte beachten Sie daher, dass Sie mit einem **KI-basierten, simulierten Patientinnenmodell** kommunizieren.
     - Zur Qualitätssicherung werden Ihre Eingaben und die Reaktionen des ChatBots auf einem Server der Universität Halle gespeichert. Persönliche Daten (incl. E-Mail-Adresse oder IP-Adresse) werden nicht gespeichert, sofern Sie diese nicht selber angeben.
     - Geben Sie daher **keine echten persönlichen Informationen** ein.
     - **Überprüfen Sie alle Angaben und Hinweise der Kommunikation auf Richtigkeit.** 
     - Die Anwendung sollte aufgrund ihrer Limitationen nur unter ärztlicher Supervision genutzt werden; Sie können bei Fragen und Unklarheiten den Chatverlauf in einer Text-Datei speichern.
 
-    Für die Richtigkeit der Inhalte kann entsprechend auch keine Haftung übernommen werden.
+    Für die Richtigkeit der Inhalte kann entsprechend keine Haftung übernommen werden.
 
     ---
     
